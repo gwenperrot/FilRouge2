4ba7ca3d2c0e3f436a1329ceb92e7205674bd802
Al <tpeedspac@gmail.com>
2017-09-13 11:04:09 +0200

    modif fenetre.java

--- FilRouge2/src/main/java/com/maven/FilRouge/presentation/Fenetre.java
+++ FilRouge2/src/main/java/com/maven/FilRouge/presentation/Fenetre.java
@@ -28,6 +28,7 @@ import com.maven.FilRouge.service.IGerant;
 
 public class Fenetre extends JFrame {
 
+	
 	private static final long serialVersionUID = -7186476640084782248L;
 
 	private IAuditeur ia = new AuditeurImpl();
